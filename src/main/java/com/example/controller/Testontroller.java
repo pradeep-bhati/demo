955b6ae9a606d1fd9a87d9d9e4d3c8f954eb6d02
pradeep.bhati <pradeep.bhati@MUMWX12268.internal.imsglobal.com>
2020-02-25 10:35:03 +0530

    added concurrency, implemented threadpool and completable future.

--- src/main/java/com/example/controller/Testontroller.java
+++ src/main/java/com/example/controller/Testontroller.java
@@ -2,17 +2,24 @@ package com.example.controller;
 
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.service.ReferenceService;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
@@ -21,38 +28,90 @@ import com.jayway.jsonpath.Option;
 @RestController
 public class Testontroller {
 	
+	@Autowired
+	private ReferenceService referenceService;
+	
 	 @PostMapping("/myapp")
-	    public String homeInit(@RequestBody String str) throws ParseException {
+	    public String homeInit(@RequestBody String str) throws ParseException, InterruptedException, ExecutionException {
 		 
+		 	long start = System.currentTimeMillis();
 	        Configuration conf = Configuration.builder().options(Option.AS_PATH_LIST).build();
-	        DocumentContext jsonContext = JsonPath.parse(str);  
+	        
 //	        System.out.println("jsonpath Object:"+jsonContext.jsonString());
 	        List<String> pathList = JsonPath.using(conf).parse(str).read("$..reference");
-	        String response = null;
-	        for(String path : pathList) {
-	            String resourceURI = jsonContext.read(path);
-            	response = getReferenceJson(resourceURI);
-            	jsonContext.set(path,JsonPath.parse(response).json());
-	        }
 	        
-	        String finaljsonstring =(jsonContext.jsonString());
+	        List<CompletableFuture<RequestResponse>> requestResponseListFutures =
+	        pathList.stream().map(path -> referenceService.getReferenceJson(path,str)).collect(Collectors.toList());
+	        CompletableFuture<Void> allFutures = CompletableFuture.allOf(
+	        		requestResponseListFutures.toArray(new CompletableFuture[requestResponseListFutures.size()])
+	        );
+	        CompletableFuture<List<RequestResponse>> allCompletableFuture = allFutures.thenApply(future -> {
+	            return requestResponseListFutures.stream()
+	                    .map(completableFuture -> completableFuture.join())
+	                    .collect(Collectors.toList());
+	        });
+	        
+	        CompletableFuture<DocumentContext> documentContextFuture = allCompletableFuture.thenApply(requestResponseList -> {
+	        	DocumentContext jsonContext = JsonPath.parse(str); 
+	        	for(RequestResponse requestResponse : requestResponseList) {
+	        		jsonContext.set(requestResponse.getPath(),JsonPath.parse(requestResponse.getResponse()).json());
+	        	}
+	        	return jsonContext;
+	        });
+	        
+	        String finaljsonstring = documentContextFuture.get().jsonString();
+	       // String response = null;
+//	        for(String path : pathList) {
+//	            String resourceURI = jsonContext.read(path);
+//            	response = getReferenceJson(resourceURI);
+//            	jsonContext.set(path,JsonPath.parse(response).json());
+//	        }
+	        
+//	        String finaljsonstring =(jsonContext.jsonString());
 //	        System.out.println("printing json finally");
 //	        System.out.println(finaljsonstring);
+//	        return finaljsonstring;
+	        long diff = System.currentTimeMillis() - start;
+	        System.out.println("time for:" +diff);
 	        return finaljsonstring;
 	    }
 	 
-	 public String getReferenceJson(String resourceURI)
-	 {
-		 System.out.println(Thread.currentThread());
-		 long start = System.currentTimeMillis();
-		 RestTemplate restTemplate = new RestTemplate();
-		 long diff = System.currentTimeMillis() - start;
-		 System.out.println("time for:"+resourceURI+" :"+diff);
-		 String baseUri = "http://hapi.fhir.org/baseR4";
-		 String uri = baseUri +"/"+ resourceURI;
-		 ResponseEntity<String> response
-		  = restTemplate.getForEntity(uri , String.class);
-		 return response.getBody();		 
+//	 @Async("asyncExecutor")
+//	 public CompletableFuture<RequestResponse>  getReferenceJson(String path,String json)
+//	 {
+//		 DocumentContext jsonContext = JsonPath.parse(json);  
+//		 String resourceURI = jsonContext.read(path);
+//		 System.out.println(Thread.currentThread());
+//		 
+//		 RestTemplate restTemplate = new RestTemplate();
+//		 
+//		 
+//		 String baseUri = "http://hapi.fhir.org/baseR4";
+//		 String uri = baseUri +"/"+ resourceURI;
+//		 ResponseEntity<String> response
+//		  = restTemplate.getForEntity(uri , String.class);
+//		 RequestResponse requestResponse = new RequestResponse();
+//		 requestResponse.setPath(path);
+//		 requestResponse.setResponse(response.getBody());
+//		 return CompletableFuture.completedFuture(requestResponse)  ;		 
+//	 }
+	 
+	 public static class RequestResponse{
+		
+		 String path;
+		 public String getPath() {
+			return path;
+		}
+		public void setPath(String path) {
+			this.path = path;
+		}
+		public String getResponse() {
+			return response;
+		}
+		public void setResponse(String response) {
+			this.response = response;
+		}
+		String response;
 	 }
 }
 	 
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,31 @@
 package com.example.demo;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@ComponentScan("com.example.controller")
+@EnableAsync
+@ComponentScan("com.example.controller,com.example.service")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+  @Bean(name = "asyncExecutor")
+  public Executor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(1000);
+    executor.setMaxPoolSize(1000);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("AsyncThread-");
+    executor.initialize();
+    return executor;
+  }
 }
